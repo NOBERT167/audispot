a715da70cda825e5ec3700c0f87858a9237ad7a5
NOBERT167 <nobertlangat0@gmail.com>
2024-10-03 13:47:58 +0300

    modified next config file

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,37 @@
-/** @type {import('next').NextConfig} */
+// next.config.mjs
+import path from "path";
+
 const nextConfig = {
   images: {
     domains: ["cdn.sanity.io"],
   },
+  webpack(config, { isServer }) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+
+    // Add custom webpack configurations here
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve("src"),
+    };
+
+    return config;
+  },
+  reactStrictMode: false,
   typescript: {
+    // Ignore TypeScript errors during the build process
     ignoreBuildErrors: true,
   },
 };
+
 export default nextConfig;
